acde922594d72da9b663151050120d6dedf0a0d2
Anthony Simmon <simmon.anthony@gmail.com>
2017-07-29 02:45:18 -0400

    Replaced tabs by 4 spaces

--- SlidingCacheAop/CachedMethodInvocation.cs
+++ SlidingCacheAop/CachedMethodInvocation.cs
@@ -5,64 +5,64 @@ using Castle.DynamicProxy;
 
 namespace SlidingCacheAop.WinApp
 {
-	public class CachedMethodInvocation
-	{
-		private IInvocation _innerInvocation;
-		private CacheResultAttribute _cacheAttribute;
+    public class CachedMethodInvocation
+    {
+        private IInvocation _innerInvocation;
+        private CacheResultAttribute _cacheAttribute;
 
-		public object ReturnValue
-		{
-			get { return _innerInvocation.ReturnValue; }
-			set { _innerInvocation.ReturnValue = value; }
-		}
+        public object ReturnValue
+        {
+            get { return _innerInvocation.ReturnValue; }
+            set { _innerInvocation.ReturnValue = value; }
+        }
 
-		public string Signature { get; private set; }
+        public string Signature { get; private set; }
 
-		public TimeSpan CacheDuration
-		{
+        public TimeSpan CacheDuration
+        {
             get { return _cacheAttribute.Duration; }
-		}
+        }
 
-		public CachedMethodInvocation(IInvocation invocation)
-		{
-			_innerInvocation = invocation;
-			ComputeSignatureIfCacheAttributeExistOnMethod();
-		}
+        public CachedMethodInvocation(IInvocation invocation)
+        {
+            _innerInvocation = invocation;
+            ComputeSignatureIfCacheAttributeExistOnMethod();
+        }
 
-		public void Proceed()
-		{
-			_innerInvocation.Proceed();
-		}
+        public void Proceed()
+        {
+            _innerInvocation.Proceed();
+        }
 
-		private void ComputeSignatureIfCacheAttributeExistOnMethod()
-		{
-			_cacheAttribute = GetCacheAttributeFromMethod();
-			if (IsCacheEnabled())
-				ComputeSignature();
-		}
+        private void ComputeSignatureIfCacheAttributeExistOnMethod()
+        {
+            _cacheAttribute = GetCacheAttributeFromMethod();
+            if (IsCacheEnabled())
+                ComputeSignature();
+        }
 
-		private CacheResultAttribute GetCacheAttributeFromMethod()
-		{
-			var targetMethod = _innerInvocation.MethodInvocationTarget;
-			var cacheAttribute = targetMethod.GetCustomAttribute<CacheResultAttribute>();
+        private CacheResultAttribute GetCacheAttributeFromMethod()
+        {
+            var targetMethod = _innerInvocation.MethodInvocationTarget;
+            var cacheAttribute = targetMethod.GetCustomAttribute<CacheResultAttribute>();
 
-			return cacheAttribute;
-		}
+            return cacheAttribute;
+        }
 
-		public bool IsCacheEnabled()
-		{
+        public bool IsCacheEnabled()
+        {
             return _cacheAttribute != null && _cacheAttribute.Duration.Ticks > 0;
-		}
+        }
 
-		private void ComputeSignature()
-		{
+        private void ComputeSignature()
+        {
             // TODO It is up to you to implement a more complex logic to generate a signature based on the arguments
             // Some complex argument cases: datetimes, enumerables, "should-not-be-serializable" objects (ex:Stream?)
-			Signature = string.Format("{0}-{1}-{2}",
-				_innerInvocation.TargetType.FullName,
-				_innerInvocation.Method.Name,
-				string.Join("-", _innerInvocation.Arguments.Select(a => (a ?? "").ToString()).ToArray())
-			);
-		}
-	}
+            Signature = string.Format("{0}-{1}-{2}",
+                _innerInvocation.TargetType.FullName,
+                _innerInvocation.Method.Name,
+                string.Join("-", _innerInvocation.Arguments.Select(a => (a ?? "").ToString()).ToArray())
+            );
+        }
+    }
 }
